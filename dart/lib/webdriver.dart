4ece1f2c5b32a90d06bed7e467d864e6eef18a05
Marc Fisher II <fisherii@google.com>
2015-02-25 11:41:44 -0800

    Style updates.

--- dart/lib/webdriver.dart
+++ dart/lib/webdriver.dart
@@ -1,30 +1,29 @@
-/*
- * Copyright 2014 Google Inc. All rights reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2014 Google Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 /// PageLoader WebDriver provides the necessary bindings to support using
 /// PageLoader in WebDriver-based tests.
 library pageloader.webdriver;
-
-import 'clock.dart';
-import 'src/core.dart';
-import 'src/interfaces.dart';
-export 'src/interfaces.dart';
 
 import 'dart:collection';
 import 'dart:io' as io;
 
 import 'package:sync_webdriver/sync_webdriver.dart' as wd;
+
+import 'clock.dart';
+import 'src/core.dart';
+import 'src/interfaces.dart';
+export 'src/interfaces.dart';
 
 class WebDriverPageLoader extends BasePageLoader {
   WebDriverPageLoaderElement _globalContext;
--- dart/test/html_no_shadow_dom_test.dart
+++ dart/test/html_no_shadow_dom_test.dart
@@ -1,27 +1,26 @@
-/*
- * Copyright 2014 Google Inc. All rights reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2014 Google Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 library pageloader.test.html_no_shadow_dom;
+
+import 'dart:html' as html;
+
+import 'package:pageloader/html.dart';
+import 'package:unittest/html_enhanced_config.dart'
+    show useHtmlEnhancedConfiguration;
+import 'package:unittest/unittest.dart';
 
 import 'page_objects.dart';
 import 'pageloader_test.dart' as plt;
-
-import 'package:pageloader/html.dart';
-import 'package:unittest/html_enhanced_config.dart';
-import 'package:unittest/unittest.dart';
-
-import 'dart:html' as html;
 
 void main() {
   useHtmlEnhancedConfiguration();
