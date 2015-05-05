861b5154a6c0594b24181ce398d7d8ac3912d9ef
Marc Fisher II <fisherii@google.com>
2015-05-04 17:32:58 -0700

    Remove outdated comment.

--- dart/test/sync/pageloader_test.dart
+++ dart/test/sync/pageloader_test.dart
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-@Skip() // disable these tests with impossible constraint
+@Skip()
 library pageloader.sync.test;
 
 import 'package:matcher/matcher.dart';
