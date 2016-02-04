01ee23c718180d9f02545b52372b0fd89af601e7
Michael Goderbauer <goderbauer@google.com>
2016-02-04 13:46:51 +0000

    Divide async and sync pageloader into two packages Also: run browser tests on travis

---
+++ dart/async/test/src/basic.dart
@@ -0,0 +1,253 @@
+// Copyright 2014 Google Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+library pageloader.test.basic;
+
+import 'package:pageloader/objects.dart';
+import 'package:test/test.dart';
+import 'shared.dart';
+
+void runTests() {
+  group('basic tests', () {
+    test('simple', () async {
+      PageForSimpleTest page = await loader.getInstance(PageForSimpleTest);
+      await verifyTable(page.table);
+      expect(page.loader, loader);
+    });
+
+    test('class annotations', () async {
+      Table table = await loader.getInstance(Table);
+      await verifyTable(table);
+    });
+
+    test('class annotation on nested field', () async {
+      PageForClassAnnotationTest page =
+          await loader.getInstance(PageForClassAnnotationTest);
+      await verifyTable(page.table);
+    });
+
+    test('sub-class', () async {
+      SubclassPage page = await loader.getInstance(SubclassPage);
+      await verifyTable(page.table);
+    });
+
+    test('setters', () async {
+      PageForSettersTest page = await loader.getInstance(PageForSettersTest);
+      expect(page.shouldHaveOneElement, hasLength(1));
+      expect(page.loader, loader);
+    });
+
+    test('skip fields without finders', () async {
+      PageForSkipFieldsWithoutFinderTest page =
+          await loader.getInstance(PageForSkipFieldsWithoutFinderTest);
+      expect(page.shouldHaveOneElement, hasLength(1));
+      expect(page.shouldBeNull, isNull);
+    });
+
+    test('mixin', () async {
+      PageForMixinTest page = await loader.getInstance(PageForMixinTest);
+      await verifyTable(page.table);
+      expect(page.shouldHaveOneElement, hasLength(1));
+      expect(page.shouldBeEmpty, isEmpty);
+      expect(page.shouldAlsoBeEmpty, isEmpty);
+    });
+
+    test('private constructor', () async {
+      PageForPrivateConstructorTest page =
+          await loader.getInstance(PageForPrivateConstructorTest);
+
+      await verifyTable(page.table);
+    });
+
+    test('private fields', () async {
+      PageForPrivateFieldsTest page =
+          await loader.getInstance(PageForPrivateFieldsTest);
+      await verifyTable(page.table);
+    });
+
+    test('Lazy fields', () async {
+      PageForLazyTest page = await loader.getInstance(PageForLazyTest);
+
+      expect(await (await page.lazyElement()).visibleText, contains('r1c1'));
+      await verifyTable(await page.lazyTable());
+
+      expect(await page.lazyRowElements(), hasLength(2));
+      await verifyRows(await page.lazyRows());
+    });
+
+    test('has focus', () async {
+      PageForFocusTest page = await loader.getInstance(PageForFocusTest);
+
+      expect(await page.textfield.isFocused, false);
+
+      await page.textfield.focus();
+      expect(await page.textfield.isFocused, true);
+
+      await page.textfield.blur();
+      expect(await page.textfield.isFocused, false);
+    }, onPlatform: {
+      'content-shell': new Skip('Focus events do not work in content-shell')
+    });
+
+    test('classes', () async {
+      PageForSimpleTest page = await loader.getInstance(PageForSimpleTest);
+
+      expect(await page.table.table.classes.toList(),
+          orderedEquals(['class1', 'class2', 'class3']));
+      expect(await page.table.rows.first.cells.first.classes.toList(),
+          hasLength(0));
+    });
+
+    test('style', () async {
+      PageForSimpleTest page = await loader.getInstance(PageForSimpleTest);
+
+      expect(await page.table.table.style['color'], 'rgb(128, 0, 128)');
+      expect(await page.table.table.style['backgroundColor'], '');
+    });
+
+    test('computedStyle', () async {
+      PageForSimpleTest page = await loader.getInstance(PageForSimpleTest);
+
+      expect(await page.table.table.computedStyle['color'], 'rgb(128, 0, 128)');
+      expect(await page.table.table.computedStyle['background-color'],
+          'rgb(0, 255, 0)');
+    });
+
+    test('optional with Lazy', () async {
+      PageForOptionalFunctionTest page =
+          await loader.getInstance(PageForOptionalFunctionTest);
+
+      expect(await page.shouldBeNull(), isNull);
+      expect(await page.shouldBeNonNull(), isNotNull);
+    });
+
+    test('nbsp in text', () async {
+      PageForNbspTest page = await loader.getInstance(PageForNbspTest);
+      expect(await page.span.visibleText, '   ');
+      expect(await page.span.innerText, '');
+    });
+
+    test('optional', () async {
+      PageForOptionalElementTest page =
+          await loader.getInstance(PageForOptionalElementTest);
+      expect(page.doesntExist, isNull);
+    });
+  });
+}
+
+class PageForOptionalFunctionTest {
+  @ByTagName('non-existant')
+  @optional
+  Lazy shouldBeNull;
+
+  @ById('button-1')
+  @optional
+  Lazy shouldBeNonNull;
+}
+
+class SubclassPage extends PageForSimpleTest {}
+
+class PageForClassAnnotationTest {
+  @root
+  Table table;
+}
+
+class PageForSettersTest {
+  List<PageLoaderElement> _shouldHaveOneElement;
+  var _loader;
+
+  @inject
+  set loader(PageLoader l) {
+    this._loader = l;
+  }
+
+  PageLoader get loader => _loader;
+
+  @ById('div')
+  @disableDisplayedCheck
+  set shouldHaveOneElement(List<PageLoaderElement> elements) {
+    _shouldHaveOneElement = elements;
+  }
+
+  get shouldHaveOneElement => _shouldHaveOneElement;
+}
+
+class PageForSkipFieldsWithoutFinderTest {
+  @ById('div')
+  @disableDisplayedCheck
+  List<PageLoaderElement> shouldHaveOneElement;
+
+  List<PageLoaderElement> shouldBeNull;
+}
+
+class PageForOptionalElementTest {
+  @ById('non-existent id')
+  @optional
+  PageLoaderElement doesntExist;
+}
+
+class MixinForMixinTest {
+  @ById('div')
+  @disableDisplayedCheck
+  List<PageLoaderElement> shouldHaveOneElement;
+
+  @ById('div')
+  List<PageLoaderElement> shouldBeEmpty;
+
+  @ById('div')
+  @IsDisplayed()
+  @disableDisplayedCheck
+  List shouldAlsoBeEmpty;
+}
+
+class PageForAmbiguousTest {
+  @ByTagName('input')
+  PageLoaderElement element;
+}
+
+class PageForMixinTest extends PageForSimpleTest with MixinForMixinTest {}
+
+class PageForPrivateConstructorTest extends PageForSimpleTest {
+  PageForPrivateConstructorTest._();
+}
+
+class PageForPrivateFieldsTest {
+  @ByTagName('table')
+  Table _privateTable;
+
+  Table get table => _privateTable;
+}
+
+class PageForLazyTest {
+  @ByTagName('table')
+  Lazy lazyElement;
+
+  @ByTagName('table')
+  Lazy<Table> lazyTable;
+
+  @ByCss('table tr')
+  Lazy<List> lazyRowElements;
+
+  @ByCss('table tr')
+  Lazy<List<Row>> lazyRows;
+}
+
+class PageForNbspTest {
+  @ById('nbsp')
+  PageLoaderElement span;
+}
+
+class PageForFocusTest {
+  @ById('text')
+  PageLoaderElement textfield;
+}
---
+++ dart/async/test/webdriver_no_shadow_dom_test.dart
@@ -0,0 +1,76 @@
+// Copyright 2014 Google Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+@TestOn('vm')
+library pageloader.test.webdriver;
+
+import 'dart:async';
+import 'dart:io';
+
+import 'package:pageloader/webdriver.dart' show WebDriverPageLoader;
+import 'package:path/path.dart' as path;
+import 'package:test/test.dart';
+import 'package:webdriver/io.dart' show Capabilities, WebDriver, createDriver;
+
+import 'src/common.dart' as plt;
+import 'src/shared.dart' as shared;
+
+void main() {
+  WebDriver driver;
+
+  setUp(() async {
+    driver = await _createTestDriver();
+    await driver.get(_testPagePath);
+    shared.loader = new WebDriverPageLoader(driver, useShadowDom: false);
+  });
+
+  tearDown(() async {
+    await driver.quit();
+    shared.loader = null;
+  });
+
+  plt.runTests();
+}
+
+String get _testPagePath {
+  var testPagePath =
+      path.join('test', 'data', 'webdriver_no_shadow_dom_test_page.html');
+
+  testPagePath = path.absolute(testPagePath);
+  if (!FileSystemEntity.isFileSync(testPagePath)) {
+    throw new Exception('Could not find the test file at "$testPagePath".'
+        ' Make sure you are running tests from the root of the project.');
+  }
+  return path.toUri(testPagePath).toString();
+}
+
+Future<WebDriver> _createTestDriver() {
+  Map capabilities = Capabilities.chrome;
+  Map env = Platform.environment;
+
+  Map chromeOptions = {};
+
+  if (env['CHROMEDRIVER_BINARY'] != null) {
+    chromeOptions['binary'] = env['CHROMEDRIVER_BINARY'];
+  }
+
+  if (env['CHROMEDRIVER_ARGS'] != null) {
+    chromeOptions['args'] = env['CHROMEDRIVER_ARGS'].split(' ');
+  }
+
+  if (chromeOptions.isNotEmpty) {
+    capabilities['chromeOptions'] = chromeOptions;
+  }
+
+  return createDriver(desired: capabilities);
+}
