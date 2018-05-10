2d7733e3628b089cb752684e5a423f98f9dab25c
Kevin Moore <kevmoo@google.com>
2018-05-10 15:06:55 -0700

    Use conventional name for builder: builder.dart

--- lib/builder.dart
+++ lib/builder.dart
@@ -11,6 +11,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/// Configuration for using `package:build`-compatible build systems.
+///
+/// See:
+/// * [build_runner](https://pub.dartlang.org/packages/build_runner)
+///
+/// This library is **not** intended to be imported by typical end-users unless
+/// you are creating a custom compilation pipeline. See documentation for
+/// details, and `build.yaml` for how these builders are configured by default.
+library pageloader.builder;
+
 import 'package:build/build.dart';
 import 'package:source_gen/source_gen.dart';
 
