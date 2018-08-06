e5cde0c0d7ba7e9a09d6186a2d3de08f2ef707f4
Kevin Moore <kevmoo@google.com>
2018-08-06 12:05:10 -0700

    test: annotate vm-only tests as such
    Allows 'pub run test -p chrome'

--- test/core_method_information_test.dart
+++ test/core_method_information_test.dart
@@ -11,6 +11,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+@TestOn('vm')
+
 import 'package:pageloader/src/generators/methods/core_method_information.dart';
 import 'package:test/test.dart';
 
--- test/correct_gen_test.dart
+++ test/correct_gen_test.dart
@@ -11,6 +11,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+@TestOn('vm')
+
 import 'package:test/test.dart';
 
 import 'examples/correct/class_checks.dart';
