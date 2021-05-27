e14484dfe519005e92b1cffcd2512f5bcc45663c
Max Kim <maxkim@google.com>
2021-05-27 13:24:43 -0700

    Fix ->

--- lib/src/api/page_loader_source_interface.dart
+++ lib/src/api/page_loader_source_interface.dart
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-library pageloader3.api.page_loader_source_interface;
+library pageloader.api.page_loader_source_interface;
 
 import 'page_loader_element_interface.dart';
 
